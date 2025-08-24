0335824a83ae1554c471e7f86613fcddd7385eb7
at1shay24 <atishay01082006@gmail.com>
2025-08-24 15:55:10 -0500

    updated Player class: added in_jail, move handles passing Go, buy_property uses Property object, added __str__ for easier display

--- player.py
+++ player.py
@@ -4,9 +4,16 @@ class Player:
         self.money = money
         self.position = 0
         self.properties = []
+        self.in_jail = False
 
     def move(self, steps):
+        old_position = self.position
         self.position = (self.position + steps) % 40
+
+        # Passed 'Go'
+        if self.position < old_position:
+            self.money += 200
+            print(f"{self.name} passed Go and collected $200!")
 
     def pay(self, amount):
         self.money -= amount
@@ -14,9 +21,12 @@ class Player:
     def receive(self, amount):
         self.money += amount
 
-    def buy_property(self, property_name, price):
-        if self.money >= price:
-            self.money -= price
-            self.properties.append(property_name)
+    def buy_property(self, property_obj):
+        if self.money >= property_obj.price:
+            self.money -= property_obj.price
+            self.properties.append(property_obj)
             return True
         return False
+    
+    def __str__(self):
+        return f"{self.name}: ${self.money}, Position {self.position}, Properties: {[p.name for p in self.properties]}"
